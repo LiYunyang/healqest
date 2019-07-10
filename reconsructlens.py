6882127088447b6211739f32ac7d4bf127d2e3de
yomori <yomori@physics.mcgill.ca>
2019-07-09 23:14:35 -0500

    update weights

--- reconsructlens.py
+++ reconsructlens.py
@@ -1,12 +1,42 @@
 import utils
 import weights
 import qest
+import sys
 
-lmax = 1000
+seed = int(sys.argv[1])
 
-almT =
-almE =
+'''
+for seed in range(11,50):
+    t,q,u=hp.read_map('lensedTQU1phi1_planck2018_base_plikHM_TTTEEE_lowl_lowE_lensing_seed%d_lmax9000_nside8192_interp1.0_method1_pol_1_lensed_map.fits'%seed,field=[0,1,2])
+    almT,almE,almB=hp.map2alm([t,q,u],lmax=4096)
+    hp.write_alm('almT_seed%d.alm'%seed,almT) 
+    hp.write_alm('almE_seed%d.alm'%seed,almE) 
+    hp.write_alm('almB_seed%d.alm'%seed,almB) 
+'''
 
-glm,clm,resp = qest.qest('TT',lmax,almT,almT) #reconstruct TT lensing 
+fwhm      = 1.
+nlevT     = 2.2
+nlevP     = 2.2*2**0.5
+bl        = hp.gauss_beam(fwhm=fwhmT*0.000290888,lmax=lmax)
+nltt      = (np.pi/180./60.*nlevT)**2/bl**2
+nlee=nlbb = (np.pi/180./60.*nlevP)**2/bl**2
+
+almT  = hp.read_alm('/project2/chihway/yuuki/3gtests/alms/almT_seed%d.alm'%seed)
+almE  = hp.read_alm('/project2/chihway/yuuki/3gtests/alms/almE_seed%d.alm'%seed)
+almB  = hp.read_alm('/project2/chihway/yuuki/3gtests/alms/almB_seed%d.alm'%seed)
+
+t,q,u = hp.alm2map([almT,almE,almB],2048)
+nmapt = hp.synfast(nltt,2048)
+nmapq = hp.synfast(nlee,2048)
+nmapu = hp.synfast(nlbb,2048)
+
+almT,almE,almB = hp.map2alm([t+nmapt,q+nmapq,u+nmapu],lmax=4096)
+
+# ----------- reconstructing lens ---------------
+lmax = 4096
+
+glmTT,clmTT = qest.qest('TT',lmax,almT,almT) #reconstruct TT lensing 
+glmTE,clmTE = qest.qest('TE',lmax,almT,almE) #reconstruct TT lensing 
+glmEE,clmEE = qest.qest('EE',lmax,almE,almE) #reconstruct TT lensing 
 
 
--- weights.py
+++ weights.py
@@ -11,8 +11,8 @@ class weights():
             self.wl = { i : {} for i in range(0, self.ntrm) }
             self.sl = { i : {} for i in range(0, self.ntrm) }
             f1 = -0.5*np.ones_like(l)
-            f2 = np.sqrt(l*(l+1))
-            f3 = np.sqrt(l*(l+1))*sltt[:lmax+1]; f3[:3]=0
+            f2 = np.nan_to_num(np.sqrt(l*(l+1)))
+            f3 = np.nan_to_num(np.sqrt(l*(l+1)))*sltt[:lmax+1]
             self.w[0][0]=f3; self.w[0][1]=f1; self.w[0][2]=f2; self.s[0][0]=+1; self.s[0][1]=+0; self.s[0][2]=+1
             self.w[1][0]=f3; self.w[1][1]=f1; self.w[1][2]=f2; self.s[1][0]=+1; self.s[1][1]=+0; self.s[1][2]=+1
             self.w[2][0]=f1; self.w[2][1]=f3; self.w[2][2]=f2; self.s[2][0]=+0; self.s[2][1]=-1; self.s[2][2]=-1
@@ -25,9 +25,9 @@ class weights():
             self.sl = { i : {} for i in range(0, self.ntrm) }
             l  = np.arange(lmax+1,dtype=np.float_)
             f1 = -0.25*np.ones_like(l)
-            f2 = +np.sqrt(l*(l+1))
-            f3 = +np.sqrt((l+2.)*(l-1.))*slee[:lmax+1]; f3[:3]=0
-            f4 = -np.sqrt((l+3.)*(l-2.))*slee[:lmax+1]; f4[:3]=0
+            f2 = +np.nan_to_num(np.sqrt(l*(l+1)))
+            f3 = +np.nan_to_num(np.sqrt((l+2.)*(l-1.)))*slee[:lmax+1]
+            f4 = -np.nan_to_num(np.sqrt((l+3.)*(l-2.)))*slee[:lmax+1]
             self.w[0][0]=f3; self.w[0][1]=f1; self.w[0][2]=f2; self.s[0][0]=-1; self.s[0][1]=+2; self.s[0][2]=+1
             self.w[1][0]=f4; self.w[1][1]=f1; self.w[1][2]=f2; self.s[1][0]=-3; self.s[1][1]=+2; self.s[1][2]=-1
             self.w[2][0]=f4; self.w[2][1]=f1; self.w[2][2]=f2; self.s[2][0]=+3; self.s[2][1]=-2; self.s[2][2]=+1
@@ -44,18 +44,39 @@ class weights():
             self.sl = { i : {} for i in range(0, self.ntrm) }
             l  = np.arange(lmax+1,dtype=np.float_)
             f1 = -0.25*np.ones_like(l,dtype=np.float_)
-            f2 =  np.sqrt(l*(l+1))
-            f3 =  np.sqrt((l+2.)*(l-1.) )*flte[:lmax+1]; f3[:2]=0
-            f4 = -np.sqrt((l+3.)*(l-2.) )*flte[:lmax+1]; f4[:2]=0
+            f2 =  np.nan_to_num(np.sqrt(l*(l+1)))
+            f3 =  np.nan_to_num(np.sqrt((l+2.)*(l-1.)))*flte[:lmax+1]
+            f4 = -np.nan_to_num(np.sqrt((l+3.)*(l-2.)))*flte[:lmax+1]
             f5 = -0.50*np.ones_like(l,dtype=np.float_)
-            f6 =  np.sqrt(l*(l+1))
-            f7 =  np.sqrt(l*(l+1))*flte[:lmax+1]
+            f6 =  np.nan_to_num(np.sqrt(l*(l+1)))
+            f7 =  np.nan_to_num(np.sqrt(l*(l+1)))*flte[:lmax+1]
             self.w[0][0]=f3; self.w[0][1]=f1; self.w[0][2]=f2; self.s[0][0]=+1; self.s[0][1]=+2; self.s[0][2]=+1
             self.w[1][0]=f4; self.w[1][1]=f1; self.w[1][2]=f2; self.s[1][0]=-3; self.s[1][1]=+2; self.s[1][2]=-1
             self.w[2][0]=f4; self.w[2][1]=f1; self.w[2][2]=f2; self.s[2][0]=+3; self.s[2][1]=-2; self.s[2][2]=+1
             self.w[3][0]=f3; self.w[3][1]=f1; self.w[3][2]=f2; self.s[3][0]=+1; self.s[3][1]=-2; self.s[3][2]=-1
             self.w[4][0]=f5; self.w[4][1]=f7; self.w[4][2]=f6; self.s[4][0]=+0; self.s[4][1]=-1; self.s[4][2]=-1
             self.w[5][0]=f5; self.w[5][1]=f7; self.w[5][2]=f6; self.s[5][0]=+0; self.s[5][1]=+1; self.s[5][2]=+1
+
+        if est=='EB':
+            self.slee = slee
+            self.ntrm = 6
+            self.wl = { i : {} for i in range(0, self.ntrm) }
+            self.sl = { i : {} for i in range(0, self.ntrm) }
+            f1 =  (+1/(4j)*np.ones_like(l))
+            f2 =  (+1/(4j)*np.ones_like(l))
+            f3 = +np.nan_to_num(np.sqrt(l*(l+1)))
+            f4 = +np.nan_to_num(np.sqrt((l+2.)*(l-1.)))*slee[:lmax+1]
+            f5 = -np.nan_to_num(np.sqrt((l+3.)*(l-2.)))*slee[:lmax+1]
+            f6 = +np.nan_to_num(np.sqrt((l+2.)*(l-1.)))*slbb[:lmax+1]
+            f7 = -np.nan_to_num(np.sqrt((l+3.)*(l-2.)))*slbb[:lmax+1]         
+            self.w[0][0]=f4; self.w[0][1]=f1; self.w[0][2]=f3; self.s[0][0]=-1; self.s[0][1]=+2; self.s[0][2]=+1
+            self.w[1][0]=f5; self.w[1][1]=f1; self.w[1][2]=f3; self.s[1][0]=-3; self.s[1][1]=+2; self.s[1][2]=-1
+            self.w[2][0]=f5; self.w[2][1]=f2; self.w[2][2]=f3; self.s[2][0]=+3; self.s[2][1]=-2; self.s[2][2]=+1
+            self.w[3][0]=f4; self.w[3][1]=f2; self.w[3][2]=f3; self.s[3][0]=+1; self.s[3][1]=-2; self.s[3][2]=-1
+            self.w[4][0]=f6; self.w[4][1]=f2; self.w[4][2]=f3; self.s[4][0]=-2; self.s[4][1]=+1; self.s[4][2]=-1
+            self.w[5][0]=f7; self.w[5][1]=f2; self.w[5][2]=f3; self.s[5][0]=-2; self.s[5][1]=+3; self.s[5][2]=+1
+            self.w[6][0]=f7; self.w[6][1]=f1; self.w[6][2]=f3; self.s[6][0]=+2; self.s[6][1]=-3; self.s[6][2]=-1
+            self.w[7][0]=f6; self.w[7][1]=f1; self.w[7][2]=f3; self.s[7][0]=+2; self.s[7][1]=-1; self.s[7][2]=+1
 
 
 '''

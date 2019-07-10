8d490eac26ccdd6db5ce5b7475ff3bdf50ad08e2
yomori <yomori@physics.mcgill.ca>
2019-07-09 23:36:42 -0500

    update

--- qest.py
+++ qest.py
@@ -6,9 +6,8 @@ def qest(est,lmax,clfile,almbar1,almbar2):
     retglm  = 0
     retclm  = 0
     nside    = utils.get_nside(lmax)
-
-    for i in range (0,ntrm):
-        w                 = weights.weights(est,lmax,clfile)
+    w        = weights.weights(est,lmax,clfile)
+    for i in range (0,w.ntrm):
         wX,wY,wP,sX,sY,sP = w[i][0],w[i][1],w[i][2],s[i][0],s[i][1],s[i][2]
         walmbar1          = hp.almxfl(almbar1,wX)#########
         walmbar2          = hp.almxfl(almbar2,wY)#########

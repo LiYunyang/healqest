3f06b486e78f09719778795c30bdd98e62a0160b
Yuuki Omori <yomori@midway2-login2.rcc.local>
2022-05-09 22:08:00 -0500

    clean up

--- setup.py
+++ setup.py
@@ -10,22 +10,9 @@ def configuration(parent_package='',top_path=None):
     return config
 
 
-#fftlog_module = Extension( name="cosmotools3.ext.pycl2xi.pycl2xi._fftlog",
-#                           sources=["cosmotools3/ext/pycl2xi/src/fftlog.c"],
-#                           depends=["cosmotools3/ext/pycl2xi/src/fftlog.h"],
-#                           libraries=["fftw3"],
-#                           extra_compile_args=["-std=gnu99"]
-#                           )
-
 if __name__ == "__main__":
     from numpy.distutils.core import setup
 
-    packages     = []#["cosmotools3","cosmotools3.ext","cosmotools3.ext.quicklens","cosmotools3.ext.pycl2xi","cosmotools3.ext.pycl2xi.pycl2xi","cosmotools3.ext.pycl2xi.src"]
+    packages     = []
 
-    #data_dir     = os.path.join("data","cl","planck_wp_highL","*")
-    #package_data = { "cosmotools3" : [data_dir] }
-
-    setup(packages      = packages,
-          #package_data  = package_data,
-          #ext_modules   = [fftlog_module],
-          configuration = configuration)
+    setup(packages = packages, configuration = configuration)

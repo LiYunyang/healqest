0a567d9d0be570b2cae149de9f7b980dcf163cfe
YL <20972148+LiYunyang@users.noreply.github.com>
2025-01-19 21:51:52 -0600

    [install] build with setuptools
    `distutils` not supported in py312 onward.

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import os,glob
 import numpy as np
 import distutils
+import subprocess
 from distutils.extension import Extension
+from setuptools.command.build_ext import build_ext
 
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
@@ -10,9 +12,40 @@ def configuration(parent_package='',top_path=None):
     return config
 
 
+class BuildF2Py(build_ext):
+    def run(self):
+        F2PY = os.environ.get("F2PY", "f2py")
+        # Compile with f2py
+        subprocess.run(
+            [
+                F2PY,
+                "-c",
+                "-m",
+                "healqest.src.cwignerd",
+                "healqest/src/wignerd.pyf",
+                "healqest/src/wignerd.c",
+            ],
+            check=True,
+        )
+
+        outdir = os.path.join("healqest", "src")
+        subprocess.run(
+            f"mv cwignerd*.so {outdir}",
+            shell=True,
+        )
+
+
 if __name__ == "__main__":
-    from numpy.distutils.core import setup
-
-    packages     = []
-
-    setup(packages = packages, configuration = configuration)
+    try:
+        from numpy.distutils.core import setup
+        packages = []
+        setup(packages=packages, configuration=configuration)
+    except Exception:
+        # alternative build for py>3.12 or numpy>1.23
+        # using setuptools + meson based f2py.
+        from setuptools import setup
+        setup(
+            name="healqest",
+            packages=["healqest", "healqest.src"],
+            cmdclass={"build_ext": BuildF2Py}
+        )

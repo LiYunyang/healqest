e0ea5c119a06d61911b4765c8a8c78b2bfd17ebb
YL <20972148+LiYunyang@users.noreply.github.com>
2025-04-18 09:43:40 +0800

    [feat] mask and argparse handling in startup

--- healqest/startup.py
+++ healqest/startup.py
@@ -1,6 +1,8 @@
 import sys
-from dataclasses import dataclass
+import argparse
 import os
+from typing import Union
+
 import numpy as np
 import yaml
 from healqest import utils
@@ -20,10 +22,18 @@ except ImportError:
 
 class Config:
     outdir: str  # output directory
-    recdir: str = None # output directory for lensing rec. Default to outdir
+    recdir: str = None  # output directory for lensing rec. Default to outdir
     mfsplit: bool
+    field: str
+    """
+    the SPT field identifier. This is used to format `outdir`/`recdir`/`dec_range` and mask files.
+    """
+    nside: int  # used for ducc wrapper
 
-    nside: int # used for ducc wrapper
+    fmask_qe: Union[str, list[str]]=None  # path(s) to mask used for lensrec
+    fmask_ps: Union[str, list[str]]=None  # path(s) to mask used for MC resp and clpp
+    dec_range: Union[list, dict]=None
+    spice_kwargs: dict=None  # polspice settings
 
     def __init__(self, **kwargs):
         # TODO: this should really be a dataclass with all possible arguments
@@ -38,11 +48,25 @@ class Config:
         self._dict_cls = None
         if self.recdir is None:
             self.recdir = self.outdir
+        if self.field is not None:
+            self.outdir = self.outdir.format(field=self.field)
+            self.recdir = self.recdir.format(field=self.field)
+        if isinstance(self.dec_range, dict):
+            if self.field is not None:
+                self.dec_range = self.dec_range[self.field]
+
+        # auto adjust spice kwargs
+        if self.spice_kwargs:
+            for key in ['apodizesigma', 'thetamax']:
+                if self.spice_kwargs.get(key, None) == 'dec':
+                    self.spice_kwargs[key] = max(self.dec_range)-min(self.dec_range)
+                elif isinstance(self.spice_kwargs[key], dict):
+                    self.spice_kwargs[key] = self.spice_kwargs[key][self.field]
 
     @classmethod
-    def from_yaml(cls, fname, pipeline=False):
+    def from_yaml(cls, fname, pipeline=False, field=None):
         params = yaml.safe_load(open(fname, "r"))
-        obj = cls(**params)
+        obj = cls(**params, field=field)
         # copy the config file
         if pipeline and rank == 0:
             os.makedirs(obj.file(obj.outdir), exist_ok=True)
@@ -66,9 +90,9 @@ class Config:
         path : str
             The path to the file. If it starts with "/", it is considered an absolute path.
             Otherwise, it is considered relative to the data directory ($HEALQEST_DATA_DIR).
-        *args : str
+        args: str
             Additional path components to be joined to the path.
-        **kwargs : dict
+        kwargs:
             Dictionary to be used for formatting the path.
         """
         if path is None:
@@ -132,10 +156,8 @@ class Config:
                 _alm = hp.read_alm(f)
                 alm += np.array([_alm, _alm, _alm])
 
-        if self.sim_mask is not None:
-            # nside = hp.get_nside(self.sim_mask)
-            # alm = hp.map2alm(hp.alm2map(alm, nside=nside, )*self.sim_mask, iter=0)
-            alm = self._mask_alm(alm, self.sim_mask)
+        if self.mask_qe is not None:
+            alm = self._mask_alm(alm, self.mask_qe)
         return alm
 
     @property
@@ -165,10 +187,31 @@ class Config:
     def g(self):
         return Geometry(nside=self.nside, dec_range=getattr(self, 'dec_range', None))
 
+    def _load_mask(self, item):
+        if isinstance(item, str):
+            return hp.read_map(self.file(item, field=self.field))
+        elif isinstance(item, list):
+            mask = None
+            for _ in item:
+                _mask = self._load_mask(_)
+                if mask is None:
+                    mask = _mask
+                else:
+                    mask = np.logical_or(mask, _mask)
+        else:
+            raise TypeError(f'Undefined item type {type(item)}')
+
     @cached_property
-    def sim_mask(self):
-        if self['lensing'].get('sim_mask', None):
-            return hp.read_map(self.file(self['lensing'].get('sim_mask')))
+    def mask_qe(self):
+        if self.fmask_qe:
+            return self._load_mask(self.fmask_qe)
+        else:
+            return None
+
+    @cached_property
+    def mask_ps(self):
+        if self.fmask_ps:
+            return self._load_mask(self.fmask_ps)
         else:
             return None
 
@@ -234,3 +277,18 @@ class Config:
 
     def _mask_alm(self, alm, mask):
         return self.g.map2alm(self.g.alm2map(alm)*mask)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', default=None, type=str, help='path to config file', required=True)
+    parser.add_argument('-f', '--field', default=None, type=str, help='SPT field')
+    parser.add_argument('-i1', default=1, type=int, help='seed start')
+    parser.add_argument('-i2', default=1, type=int, help='seed stop')
+    parser.add_argument('-cmbid1', default=1, type=int, help='cmbid1')
+    parser.add_argument('-cmbid2', default=1, type=int, help='cmbid2')
+    parser.add_argument('-N1', action='store_true', help='do N1-type sims')
+    parser.add_argument('-qe', default=None, type=str, help='QE type')
+    parser.add_argument('-qe2', default=None, dest='qe2', help='2nd QE if xpec')
+    args = parser.parse_args()
+    return args

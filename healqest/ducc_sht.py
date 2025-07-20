[fix] fix tf2d computation

healqest/ducc_sht.py
@@ -185,7 +185,7 @@ class Geometry:
         return maps
 
     def map2alm(self, maps, lmax=None, mmax=None, iter=0, pol=True, use_weights=False, use_pixel_weights=False,
-                nthreads=None,  rtol=1e-5, check=True, alms=None, **kwargs):
+                nthreads=None,  rtol=1e-5, check=True, alms=None, margin=0, **kwargs):
         """
         Computes the alm of a Healpix map. The input maps must all be in ring ordering.
 
@@ -220,7 +220,11 @@ class Geometry:
             The output alms buffer.
         """
         assert hp.get_nside(maps) == self.nside
-
+        if margin:
+            lmax0 = lmax
+            lmax = lmax0 + margin
+        else:
+            lmax0 = None
         maps = self.format_maps(maps, use_weights=use_weights, use_pixel_weights=use_pixel_weights, check=check)
         nmaps = maps.shape[0]
         dtype = maps.dtype
@@ -246,7 +250,11 @@ class Geometry:
                 func(map=maps[:1], spin=0, alm=alms[:1], **kw, **kwargs)
         if not iter:
             alms *= self.pixelarea
-        return np.squeeze(alms)
+
+        out = np.squeeze(alms)
+        if lmax0 is not None:
+            out = reduce_lmax(out, lmax=lmax0)
+        return out
 
     def alm2map(self, alms, lmax=None, mmax=None, pol=True, nthreads=None, maps=None, **kwargs):
         """
@@ -447,3 +455,22 @@ def get_dec_range(mask, dec=None):
     dec1 = int(np.floor(np.min(dec[mask > 0])))
     dec2 = int(np.ceil(np.max(dec[mask > 0])))
     return dec1, dec2
+
+
+def reduce_lmax(alm, lmax=4000):
+    """
+    Reduce the lmax of input alm
+    """
+    lmaxin = hp.Alm.getlmax(alm.shape[-1])
+    logger.debug(f"Reducing lmax: lmax_in={lmaxin} -> lmax_out={lmax}")
+    almout = np.zeros((*alm.shape[:-1], hp.Alm.getsize(lmax)), dtype=alm.dtype)
+    oldi = 0
+    newi = 0
+    dl = lmaxin - lmax
+    for i in range(0, lmax + 1):
+        oldf = oldi + lmaxin + 1 - i
+        newf = newi + lmax + 1 - i
+        almout[..., newi:newf] = alm[..., oldi:oldf - dl]
+        oldi = oldf
+        newi = newf
+    return almout

healqest/startup.py
@@ -24,7 +24,7 @@ import healpy as hp
 import yaml
 from git import Repo, InvalidGitRepositoryError
 
-from healqest import utils, healqest_utils
+from healqest import utils, healqest_utils as hq
 from healqest.ducc_sht import Geometry
 
 try:
@@ -283,7 +283,7 @@ class Config:
         if args:
             out = os.path.join(out, *args)
         if kwargs:
-            out = healqest_utils.EvalFormatter().format(out, **{k: v for k, v in kwargs.items() if v is not None})
+            out = hq.EvalFormatter().format(out, **{k: v for k, v in kwargs.items() if v is not None})
             # out = out.format(**{k: v for k, v in kwargs.items() if v is not None})
         if os.path.exists(out):
             pass
@@ -333,11 +333,11 @@ class Config:
     def mvtype2qe(mvtype):
         # SQE type MVs
         if mvtype in ['MV', 'qMV', 'PP', 'qPP', 'TTEETE', 'qTTEETE']:
-            return healqest_utils.get_qes(mvtype)
+            return hq.get_qes(mvtype)
         elif mvtype in ['TT', 'TE', 'TB', 'EE', 'EB', 'ET', 'BT', 'BE']:
-            return healqest_utils.get_qes(mvtype)
+            return hq.get_qes(mvtype)
         elif mvtype in ['TBEB', 'qTBEB', 'TEET','EBBE']:
-            return healqest_utils.get_qes(mvtype)
+            return hq.get_qes(mvtype)
         else:
             raise ValueError(f'Undefined mvtype: {mvtype}')
 
@@ -415,6 +415,8 @@ class Config:
                 alm_mask = np.ones_like(emm, dtype=float)
                 alm_mask[ell<l] = 0
                 alm_mask[emm<m] = 0
+                _, _, k = hq.dec2tf2d(0, *self.dec_range)
+                alm_mask[emm>k*ell] = 0
                 return dict(t=alm_mask.copy(), p=alm_mask.copy())
             else:
                 raise NotImplementedError
@@ -427,7 +429,8 @@ class Config:
         assert s in ['t', 'p']
         if self.tf2d:
             logger.info("computing 1d TFxbl from 2d TFxbl")
-            out = np.sqrt(hp.alm2cl(self.tfbl_2d(s)))
+            _, _, k = hq.dec2tf2d(0, *self.dec_range)
+            out = np.sqrt(hp.alm2cl(self.tfbl_2d(s))/k)
         else:
             out = self.bl[s].copy()
             if self.tf1d is not None:
@@ -752,10 +755,10 @@ class Maps(MapsBase):
             f_slm = self.config.path(self.file_cmb_tmp, seed=seed, cmbid=cmbid, bundle=bundle, field=self.field)
         logger.debug(f"loading signal sim from {f_slm}")
         try:
-            alms = utils.reduce_lmax(np.atleast_2d(hp.read_alm(f_slm, hdu=hdu)), lmax=self.lmax)
+            alms = hq.reduce_lmax(np.atleast_2d(hp.read_alm(f_slm, hdu=hdu)), lmax=self.lmax)
         except (ValueError, IndexError):
             logger.warning(f"reading map as alm failed, trying reading as maps.")
-            maps = healqest_utils.read_map(f_slm, field=np.array(hdu)-1, dtype=np.float64, use_hp=False)
+            maps = hq.read_map(f_slm, field=np.array(hdu)-1, dtype=np.float64, use_hp=False)
             alms = g.map2alm(maps, lmax=self.lmax, pol=True, check=False)
 
         if self.config.tf1d is not None:
@@ -781,10 +784,10 @@ class Maps(MapsBase):
                 del nlm
             else:
                 if 't' in pol:
-                    alms[0] += utils.reduce_lmax(self.add_noise_t(seed=seed, cmbid=cmbid, bundle=bundle,
+                    alms[0] += hq.reduce_lmax(self.add_noise_t(seed=seed, cmbid=cmbid, bundle=bundle,
                                                                   N1=self.N1), lmax=self.lmax)
                 if 'p' in pol:
-                    alms[-2:] += utils.reduce_lmax(self.add_noise_p(seed=seed, cmbid=cmbid, bundle=bundle,
+                    alms[-2:] += hq.reduce_lmax(self.add_noise_p(seed=seed, cmbid=cmbid, bundle=bundle,
                                                                     N1=self.N1), lmax=self.lmax)
         else:
             pass